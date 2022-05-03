c44a0975365f8b930491acc9eaebaad205407dce
floKianB <kianbaban@yahoo.com>
2022-05-03 02:29:25 -0400

    notes are adding and also saved in localStorage

--- src/Home/main.js
+++ src/Home/main.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './mainPage.module.css';
 import Note from './modules/eachNote';
 import Header from './modules/header/header';
 import CopyRight from './modules/footer/footer';
 
 const MainPage = () => {
-    const allNotesArr = JSON.parse(localStorage.getItem('storage'));
+    const [allNotes, setAllNotes] = useState(JSON.parse(localStorage.getItem("storage")) === null ? [] : JSON.parse(localStorage.getItem("storage")));
+    const [resultTitle, setResultTitle] = useState("");
+    const [resultNote, setResultNote] = useState("");
+    const [bgColor, setBgColor] = useState("#000000");
+    const [color, setColor] = useState("#ffffff");
 
-    return(
+    const save = () => {
+        const newSchema = {
+            title: resultTitle,
+            note: resultNote,
+            bgColor: bgColor,
+            color: color,
+        }
+        setAllNotes(allNotes => [...allNotes, newSchema]);
+        
+    }
+    console.log(allNotes);
+
+    useEffect(() => {
+        localStorage.setItem("storage", JSON.stringify(allNotes))
+    }, [allNotes]);
+    
+    return (
         <div className={styles.MainPage}>
             <Header />
+            <div className={styles.inputDiv}>
+                <input type="text" class={styles.input} id={styles.inputTitle} onInput={event => setResultTitle(event.target.value)} placeholder="Title" />
+                <input type="text" class={styles.input} onInput={event => setResultNote(event.target.value)} placeholder="Add Note ..." />
+                <input type="color" onInput={event => setBgColor(event.target.value)}/>
+                <input type="color" onInput={event => setColor(event.target.value)}/>
+                <button id={styles.submit} onClick={save}> Add</button>
+            </div>
+
             {
-                allNotesArr.map((notesObj, index) => {
-                    const title = notesObj.title;
-                    const note = notesObj.note;
-                    console.log(title + note);
-                    return <Note title={title} note={note} key={index} />
+                allNotes.map((notesObj, index) => {
+                    return <Note title={notesObj.title} note={notesObj.note} bgColor={notesObj.bgColor} color={notesObj.color} keyis={index} />
                 })
             }
             <CopyRight />
--- src/Home/modules/eachNote.js
+++ src/Home/modules/eachNote.js
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from './eachNote.module.css';
 
 const Note = (props) => {
+    styles.backgroundColor = props.bgColor
     return(
-        <div className={styles.note} key={props.key}>
-            <h3 className="title">{props.title}</h3>
-            <p className="text">{props.note}</p>
+        <div className={styles.note} style={{backgroundColor: props.bgColor}} key={props.keyis}>
+            <h3 className="title" style={{color: props.color}}>{props.title}</h3>
+            <p className="text" style={{color: props.color}}>{props.note}</p>
         </div>
     );
 }
