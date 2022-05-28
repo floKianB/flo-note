e5c4ac4cff7719e711cda6b6ccba9f613e1b5bd3
floKianB <kianbaban@yahoo.com>
2022-05-27 23:33:24 -0400

    three dots slice got debuged

--- src/Home/modules/eachNote/eachNote.js
+++ src/Home/modules/eachNote/eachNote.js
@@ -1,28 +1,19 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import styles from './eachNote.module.css';
 import deleteBtn from '../../source/delete.png';
 
 const Note = (props) => {
-    const ref = useRef()
-
-    useEffect(() => {
-        const recivedInnerHtml = ref.current.innerHTML;
-        ref.current.innerHTML = recivedInnerHtml.slice(0, 27).concat('...')
-    }, [props.onDelete])
-
     const open = () => {
         props.onOpen(props.id)
     }
-
     const close = () => {
         props.onDelete(props.id);
     }
-
     return(
         <div className={styles.note} style={{backgroundColor: props.bgColor}} key={props.key}>
             <img src={deleteBtn} alt="delete" className={styles.deleteBtn} onClick={close}/>
             <p className={styles.header} style={{color: props.color}} onClick={open} >{props.title}</p>
-            <p className={styles.text} style={{color: props.color}} ref={ref}>{props.note}</p>
+            <p className={styles.text} style={{color: props.color}}>{props.note.length > 27 ? props.note.slice(0, 27).concat('...') : props.note}</p>
         </div>
     );
 }
